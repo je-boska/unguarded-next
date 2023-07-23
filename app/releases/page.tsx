ab26e68bf94709503293d4900a39ac302a1ab5ab
je-boska <joneirikboska@gmail.com>
2023-07-23 14:45:44 +0200

    Set up Releases page with artwork

--- app/releases/page.tsx
+++ app/releases/page.tsx
@@ -1,16 +1,35 @@
 import Container from '@/components/Container';
 import { getAlbums } from '@/queries/albums';
+import Image from 'next/image';
 
 export default async function Releases() {
   const albums = await getAlbums();
 
   return (
     <Container>
-      {albums.items.map(({ fields: { slug, title } }, idx) => (
-        <div key={idx}>
-          <a href={'/releases/' + slug}>{title}</a>
-        </div>
-      ))}
+      <div className='grid md:grid-cols-2 gap-4'>
+        {albums.items.map(
+          ({ fields: { slug, title, artist, artwork } }, idx) => (
+            <div key={idx} className='group relative'>
+              <a href={'/releases/' + slug}>
+                <div className='absolute -z-10 group-hover:z-20 w-full h-full flex text-center items-center justify-center font-decay text-2xl'>
+                  {artist.fields.name}
+                  <br />
+                  {title}
+                </div>
+                <div className='bg-black opacity-20 h-full w-full absolute -z-10 group-hover:z-10' />
+                <Image
+                  className='w-full'
+                  src={'http:' + artwork.fields.file.url}
+                  alt=''
+                  width={400}
+                  height={400}
+                />
+              </a>
+            </div>
+          )
+        )}
+      </div>
     </Container>
   );
 }
