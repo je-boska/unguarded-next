7f1f2f78cddc2c87238d929b33c78e131328fe5c
je-boska <joneirikboska@gmail.com>
2023-07-23 21:15:36 +0200

    Render images in rich text

--- utils/richText.tsx
+++ utils/richText.tsx
@@ -1,6 +1,30 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { Document } from '@contentful/rich-text-types';
+import { BLOCKS, Block, Document, Inline } from '@contentful/rich-text-types';
+import { Asset } from 'contentful';
+import Image from 'next/image';
+
+const getAssetById = (id: string, assets: Asset[]) =>
+  assets.filter((asset) => asset.sys.id === id).pop();
+
+const options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node: Block | Inline) => {
+      return (
+        <Image
+          src={'http:' + node.data.target.fields.file.url}
+          alt=''
+          height={node.data.target.fields.file.details.image.height}
+          width={node.data.target.fields.file.details.image.width}
+        />
+      );
+    },
+  },
+};
 
 export function renderRichTextWithImages(document: Document) {
-  return <div className='rich-text'>{documentToReactComponents(document)}</div>;
+  return (
+    <div className='rich-text'>
+      {documentToReactComponents(document, options)}
+    </div>
+  );
 }
