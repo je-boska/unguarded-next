8d846a7ca13c3e70b7f27c952b5d906335425fec
je-boska <joneirikboska@gmail.com>
2023-07-24 13:51:04 +0200

    Rename LandingVisual js file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import LandingVisual from '@/components/LandingVisual';
-import { Suspense } from 'react';
 
 export default function Home() {
   return (
--- components/LandingVisual.tsx
+++ components/LandingVisual.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
-import { runLandingVisual } from '../utils/landingVisual.js';
+import { runLandingVisual } from '../utils/LandingVisual.js';
 
 export default function LandingVisual() {
   useEffect(() => {
