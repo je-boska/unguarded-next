7e5fb75dc4787472de0f4affed8ac05fea6696ca
je-boska <joneirikboska@gmail.com>
2023-07-25 14:59:59 +0200

    Generate youtube embeds from URLs in rich text

--- utils/richText.tsx
+++ utils/richText.tsx
@@ -1,10 +1,13 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { BLOCKS, Block, Document, Inline } from '@contentful/rich-text-types';
-import { Asset } from 'contentful';
+import {
+  BLOCKS,
+  Block,
+  Document,
+  Hyperlink,
+  INLINES,
+  Inline,
+} from '@contentful/rich-text-types';
 import Image from 'next/image';
-
-const getAssetById = (id: string, assets: Asset[]) =>
-  assets.filter((asset) => asset.sys.id === id).pop();
 
 const options = {
   renderNode: {
@@ -17,6 +20,20 @@ const options = {
           width={node.data.target.fields.file.details.image.width}
         />
       );
+    },
+    [INLINES.HYPERLINK]: (node: any) => {
+      if (node.data.uri.includes('https://youtu.be')) {
+        return (
+          <div className='iframe-container'>
+            <iframe
+              src={`https://www.youtube.com/embed/${node.data.uri.slice(-11)}`}
+              allowFullScreen
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            ></iframe>
+          </div>
+        );
+      }
+      return <a href={node.data.uri}>{node.content[0].value}</a>;
     },
   },
 };
