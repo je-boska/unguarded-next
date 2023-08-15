ce4890ffc12d596d5b8d8e304d2ca28d1813eb25
je-boska <joneirikboska@gmail.com>
2023-08-15 10:12:21 +0200

    Add all redirects

--- next.config.js
+++ next.config.js
@@ -6,8 +6,58 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: '/agb.html',
+        destination: '/imprint',
+        permanent: true,
+      },
+      {
+        source: '/DeflectionOfRealness.html',
+        destination: '/releases/deflection-of-realness',
+        permanent: true,
+      },
+      {
+        source: '/DeflectionOfRealnessFR.html',
+        destination: '/releases/deflection-of-realness',
+        permanent: true,
+      },
+      {
+        source: '/entangled.html',
+        destination: '/releases/unguarded-2-entangled',
+        permanent: true,
+      },
+      {
         source: '/in-deaths-eyes.html',
         destination: '/press/in-deaths-eyes',
+        permanent: true,
+      },
+      {
+        source: '/info.html',
+        destination: '/info',
+        permanent: true,
+      },
+      {
+        source: '/newsletter.html',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/piercingtouch.html',
+        destination: '/releases/unguarded-1-piercing-touch',
+        permanent: true,
+      },
+      {
+        source: '/releases.html',
+        destination: '/releases',
+        permanent: true,
+      },
+      {
+        source: '/Vestigios.html',
+        destination: '/releases/vestigios',
+        permanent: true,
+      },
+      {
+        source: '/youaretrouble.html',
+        destination: '/releases/youre-trouble-remixes',
         permanent: true,
       },
     ];
