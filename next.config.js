6bc0641f597b19696e779a5ef2758afbd960c242
je-boska <joneirikboska@gmail.com>
2023-08-15 09:57:17 +0200

    Add redirect for IDE press page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['images.ctfassets.net'],
   },
+  async redirects() {
+    returns[
+      {
+        source: '/in-deaths-eyes.html',
+        destination: '/press/in-deaths-eyes',
+        permanent: true,
+      }
+    ];
+  },
 };
 
 module.exports = nextConfig;
